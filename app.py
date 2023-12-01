3de9f28dc7c07054a78d5b8736cfc770df51e548
Ashwath <ashwathguru.s@gmail.com>
2023-12-02 00:37:48 +0530

    button position

--- app.py
+++ app.py
@@ -115,7 +115,9 @@ def reportsGPT():
         col1, col2, col3 = st.columns([1, 1, 1])
         with col1:
             st.markdown("![Alt Text](https://media.giphy.com/media/3o7TKzAJQ0lX1PBxug/giphy.gif)")
-            audio_bytes = audio_recorder(text="        ")
+            subcol1_1, subcol2_1, subcol3_1 = st.columns([1, 1, 1])
+            with subcol2_1:
+                audio_bytes = audio_recorder(text="")
         if audio_bytes:
             st.cache_data.clear()
             save_audio_file(audio_bytes, "mp3")
